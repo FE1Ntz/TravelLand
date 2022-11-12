aa0d3922e45c0b90bd02e22267e9e4c6713b53d1
FE1Ntz <zdanevych.y@gmail.com>
2022-11-12 17:01:00 +0200

    Import database

---
+++ TravelLand.Database/dbo/Tables/Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [Username]     NVARCHAR (MAX)   NOT NULL,
+    [Firstname]    NVARCHAR (MAX)   NOT NULL,
+    [Lastname]     NVARCHAR (MAX)   NOT NULL,
+    [PasswordHash] NVARCHAR (MAX)   NOT NULL,
+    [PasswordSalt] NVARCHAR (MAX)   NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
