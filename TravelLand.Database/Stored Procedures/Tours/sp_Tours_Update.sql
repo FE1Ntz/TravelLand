4cb73b04c376ecebacde75380269d4867fade51e
FE1Ntz <zdanevych.y@gmail.com>
2022-11-29 01:03:30 +0200

    Add Tours Update Delete and GetById stored procedures

---
+++ TravelLand.Database/Stored Procedures/Tours/sp_Tours_Update.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[sp_Tours_Update]
+	@Id UNIQUEIDENTIFIER,
+	@Name NVARCHAR(MAX),
+    @Price INT,
+    @Country NVARCHAR(MAX),
+    @Town NVARCHAR(MAX),
+    @PersonCount INT,
+    @StartDate DATE,
+    @EndDate Date,
+    @Duration INT,
+    @Description NVARCHAR(MAX)
+AS 
+     UPDATE [Tours] SET
+          [Name] = @Name,
+          [Price] = @Price,
+          [Country] = @Country,
+          [Town] = @Town,
+          [PersonCount] = @PersonCount,
+          [StartDate] = @StartDate,
+          [EndDate] = @EndDate,
+          [Duration] = @Duration,
+          [Description] = @Description
+     WHERE [Id] = @Id
