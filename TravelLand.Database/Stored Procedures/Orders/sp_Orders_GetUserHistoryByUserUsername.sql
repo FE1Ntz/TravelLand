6afffa84083aea89cab69c401e5c50ac99cfa3b6
eclipse <zdanevych.y@gmail.com>
2023-12-06 15:26:03 +0200

    add stored procedure for user history

---
+++ TravelLand.Database/Stored Procedures/Orders/sp_Orders_GetUserHistoryByUserUsername.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[sp_Orders_GetUserHistoryByUserUsername]
+	@Username NVARCHAR(MAX),
+	@IsPaid BIT
+AS
+	SELECT Tours.Id, 
+	Tours.Country, 
+	Tours.[Description], 
+	Tours.Duration, 
+	Tours.EndDate, 
+	Tours.IsHot, 
+	Tours.Logo, 
+	Tours.[Name], 
+	Tours.PersonCount, 
+	Tours.Price, 
+	Tours.StartDate, 
+	Tours.Town  
+	
+	From Tours
+	INNER Join Orders ON Orders.TourId = Tours.Id AND Orders.IsPaid = @IsPaid AND Orders.Username = @Username
+RETURN 0
